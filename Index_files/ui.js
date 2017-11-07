9b87629ac18276fab49564a3b90fec6810a47f05
longht1989 <thanhlong.vn09@gmail.com>
2017-11-07 15:12:01 +0700

    update box thời tiết & tiêu dùng

--- Index_files/ui.js
+++ Index_files/ui.js
@@ -1,8 +1,19 @@
-$(document).ready(function(){
-
-	var viewportWidth = $(window).width();
-	if(viewportWidth<1200) {
-		$('.scrollleftads').css({'display':'none'});
-		$('.scrollrightads').css({'display':'none'});
-	}
+$(document).ready(function() {
+    var viewportWidth = $(window).width();
+    if (viewportWidth < 1200) {
+        $('.scrollleftads').css({ 'display': 'none' });
+        $('.scrollrightads').css({ 'display': 'none' });
+    }
+});
+// Shorthand for $( document ).ready()
+$(function() {
+    var marqueeHeight = $('.zone-weather .summary').height();
+    if (marqueeHeight >= 100) {
+        $(".zone-weather .summary p").wrapAll("<marquee class='marquee' behavior= 'scroll' direction= 'up' scrollamount= '2' />");
+        // $('.marquee').attr({
+        //     behavior: scroll,
+        //     direction: up,
+        //     scrollamount: 2
+        // });
+    };
 });
